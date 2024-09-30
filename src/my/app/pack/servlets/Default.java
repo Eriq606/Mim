df3df2ad6ba3e4e882cc26409537eb0b5a2bdbce
Eriq606 <erohweltall@gmail.com>
2024-09-30 09:50:19 +0300

    removed Default.java bc useless

--- src/my/app/pack/servlets/Default.java
+++
@@ -1,9 +0,0 @@
-package my.app.pack.servlets;
-
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-
-@WebServlet(value = {"/"})
-public class Default extends HttpServlet{
-    
-}
